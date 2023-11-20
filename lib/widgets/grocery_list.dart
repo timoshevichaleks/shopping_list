c20520470b8a7eb9f4799b5206d48865516e27cb
tsimashevick <atsimashevich@elinext.com>
2023-11-20 21:07:16 +0300

    Added spinner.

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -31,6 +31,12 @@ class _GroceryListState extends State<GroceryList> {
         _error = 'Failed to fetch data. Please try again later.';
       });
     }
+    if (response.body == 'null') {
+      setState(() {
+        _isLoading = false;
+      });
+      return;
+    }
 
     final Map<String, dynamic> listData = json.decode(response.body);
     final List<GroceryItem> loadedItem = [];
